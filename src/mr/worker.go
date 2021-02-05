81fdbb11d1e8470976eb872a20da1177d4af3fa1
goblin <zhanyl2015@qq.com>
2021-02-05 18:41:08 +0800

    init

---
+++ src/mr/worker.go
@@ -0,0 +1,91 @@
+package mr
+
+import (
+	"fmt"
+	"hash/fnv"
+	"net/rpc"
+)
+
+//
+// Map functions return a slice of KeyValue.
+//
+type KeyValue struct {
+	Key   string
+	Value string
+}
+
+//
+// use ihash(key) % NReduce to choose the reduce
+// task number for each KeyValue emitted by Map.
+//
+func ihash(key string) int {
+	h := fnv.New32a()
+	h.Write([]byte(key))
+	return int(h.Sum32() & 0x7fffffff)
+}
+
+//
+// main/mrworker.go calls this function.
+//
+func Worker(mapf func(string, string) []KeyValue,
+	reducef func(string, []string) string) {
+
+	// Your worker implementation here.
+	file, err := GetTask()
+	if err != nil {
+		return
+	}
+	fmt.Println(file)
+	// uncomment to send the Example RPC to the master.
+	// CallExample()
+
+}
+
+// 获取任务
+func GetTask() (file string, err error) {
+	args := NewTaskArgs{}
+	reply := &NewTaskReply{}
+	err = call("Master.NewTask", &args, reply)
+	file = reply.task
+	return
+}
+
+//
+// example function to show how to make an RPC call to the master.
+//
+// the RPC argument and reply types are defined in rpc.go.
+//
+func CallExample() {
+
+	// declare an argument structure.
+	args := ExampleArgs{}
+
+	// fill in the argument(s).
+	args.X = 99
+
+	// declare a reply structure.
+	reply := ExampleReply{}
+
+	// send the RPC request, wait for the reply.
+	call("Master.Example", &args, &reply)
+
+	// reply.Y should be 100.
+	fmt.Printf("reply.Y %v\n", reply.Y)
+}
+
+//
+// send an RPC request to the master, wait for the response.
+// usually returns true.
+// returns false if something goes wrong.
+//
+func call(rpcname string, args interface{}, reply interface{}) error {
+	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
+	sockname := masterSock()
+	c, err := rpc.DialHTTP("unix", sockname)
+	if err != nil {
+		return err
+	}
+	defer c.Close()
+
+	return c.Call(rpcname, args, reply)
+}
